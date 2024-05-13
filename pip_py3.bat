2c5e43f3fc9eb5f252bd9951aa3a5f7670404aeb
unknown <darklemon2000@gmail.com>
2024-05-13 11:20:46 +0800

    mlflow

--- pip_py3.bat
+++ pip_py3.bat
@@ -5,11 +5,11 @@ call cd C:\Users\Public\py3\venv\Scripts
 call activate
 call python -m pip install --no-cache-dir -U pip
 call pip install --no-cache-dir -U ^
- pip-autoremove wheel build jupyterlab numpy pandas pyarrow openpyxl ^
+ pip-autoremove wheel build tqdm jupyterlab numpy pandas pyarrow openpyxl ^
  matplotlib seaborn dash scikit-learn xgboost statsmodels prophet networkx pyod ^
  apache-beam tf-keras tensorflow-cpu tensorflow-probability ydf tfcausalimpact ^
  lightning torch torchvision torchaudio torch_geometric ^
- ortools
+ ortools mlflow
 call pip check
 call deactivate
 call cd C:\Users\Public\py3_rar
