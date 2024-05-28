d26dc8fedd213ea463cbaa8dd229a27f01ef56a9
unknown <darklemon2000@gmail.com>
2024-05-28 18:04:49 +0800

    py3: remove lightning

--- pip_py3.bat
+++ pip_py3.bat
@@ -6,10 +6,9 @@ call activate
 call python -m pip install --no-cache-dir -U pip
 call pip install --no-cache-dir -U ^
  pip-autoremove wheel build tqdm jupyterlab numpy pandas pyarrow openpyxl ^
- matplotlib seaborn dash scikit-learn xgboost statsmodels prophet networkx pyod ^
- apache-beam tf-keras tensorflow-cpu tensorflow-probability ydf tfcausalimpact ^
- lightning torch torchvision torchaudio torch_geometric ^
- ortools mlflow
+ matplotlib seaborn dash scikit-learn xgboost statsmodels prophet networkx ^
+ apache-beam tf-keras tensorflow-cpu tensorflow-probability tfcausalimpact ^
+ torch torchvision torchaudio torch_geometric ortools pyod
 call pip check
 call deactivate
 call cd C:\Users\Public\py3_rar
