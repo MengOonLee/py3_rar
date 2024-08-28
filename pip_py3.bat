f3fef5faae12938d404b78ee46d5be9413d4021e
unknown <darklemon2000@gmail.com>
2024-08-28 16:56:14 +0800

    py3: a

--- pip_py3.bat
+++ pip_py3.bat
@@ -5,18 +5,16 @@ call cd C:\Users\Public\py3\venv\Scripts
 call activate
 call python -m pip install --no-cache-dir -U pip
 call pip install --no-cache-dir -U ^
- pip-autoremove wheel build setuptools tqdm jupyterlab ^
+ wheel build setuptools tqdm jupyterlab ^
  numpy pandas pyarrow openpyxl matplotlib seaborn dash ^
  scikit-learn xgboost statsmodels prophet networkx ortools ^
  tf-keras tensorflow-cpu tensorflow-probability ^
- torch torchvision torchaudio torch_geometric optuna ^
+ torch torchvision torchaudio torch_geometric llama-cpp-python ^
  langchain langgraph langchain-community langchain-chroma chromadb ^
  scrapy selenium apache-beam
 call pip install --no-cache-dir -U ^
  pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv ^
  -f https://data.pyg.org/whl/torch-2.4.0+cpu.html
-call pip install --no-cache-dir -U ^
- llama-cpp-python huggingface-hub
 call pip check
 call deactivate
 call cd C:\Users\Public\py3_rar
