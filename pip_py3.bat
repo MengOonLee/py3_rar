da14d4734ac631321f70b5a893863f74016f4120
unknown <darklemon2000@gmail.com>
2024-08-29 13:41:42 +0800

    deleted

--- pip_py3.bat
+++ pip_py3.bat
@@ -3,15 +3,15 @@ call cd C:\Users\Public\py3
 call python -m venv venv
 call cd C:\Users\Public\py3\venv\Scripts
 call activate
-call python -m pip install --no-cache-dir -U pip
+call python -m pip install --no-cache-dir -U ^
+ pip setuptools wheel build tqdm 
 call pip install --no-cache-dir -U ^
- wheel build setuptools tqdm jupyterlab ^
- numpy pandas pyarrow openpyxl matplotlib seaborn dash ^
- scikit-learn xgboost statsmodels prophet networkx ortools ^
- tf-keras tensorflow-cpu tensorflow-probability ^
- torch torchvision torchaudio torch_geometric llama-cpp-python ^
+ jupyterlab numpy pandas pyarrow openpyxl matplotlib seaborn dash ^
+ networkx apache-beam scikit-learn xgboost statsmodels prophet ^
+ tf-keras tensorflow-cpu tensorflow-probability ortools ^
+ torch torchvision torchaudio torch_geometric transformers ^
  langchain langgraph langchain-community langchain-chroma chromadb ^
- scrapy selenium apache-beam
+ scrapy selenium llama-cpp-python
 call pip install --no-cache-dir -U ^
  pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv ^
  -f https://data.pyg.org/whl/torch-2.4.0+cpu.html
