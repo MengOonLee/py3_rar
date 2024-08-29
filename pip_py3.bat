6c9affd330319a58c479a75cddcc0eb36e4dcdce
unknown <darklemon2000@gmail.com>
2024-08-29 18:49:44 +0800

    py3-a

--- pip_py3.bat
+++ pip_py3.bat
@@ -4,17 +4,19 @@ call python -m venv venv
 call cd C:\Users\Public\py3\venv\Scripts
 call activate
 call python -m pip install --no-cache-dir -U ^
- pip setuptools wheel build tqdm 
+ pip setuptools wheel build tqdm
 call pip install --no-cache-dir -U ^
  jupyterlab numpy pandas pyarrow openpyxl matplotlib seaborn dash ^
  networkx apache-beam scikit-learn xgboost statsmodels prophet ^
  tf-keras tensorflow-cpu tensorflow-probability ortools ^
- torch torchvision torchaudio torch_geometric transformers ^
- langchain langgraph langchain-community langchain-chroma chromadb ^
- scrapy selenium llama-cpp-python
-call pip install --no-cache-dir -U ^
- pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv ^
- -f https://data.pyg.org/whl/torch-2.4.0+cpu.html
+ scrapy selenium langchain langgraph langchain-chroma transformers
+call pip install --no-cache-dir ^
+ torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 ^
+ --index-url https://download.pytorch.org/whl/cpu
+call pip install --no-cache-dir ^
+ torch_geometric pyg_lib torch_scatter torch_sparse ^
+ torch_cluster torch_spline_conv ^
+ -f https://data.pyg.org/whl/torch-2.3.1+cpu.html
 call pip check
 call deactivate
 call cd C:\Users\Public\py3_rar
