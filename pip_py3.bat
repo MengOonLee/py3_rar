d319e753e8618b584c171f55e3b43b4310025978
unknown <darklemon2000@gmail.com>
2024-10-30 15:12:59 +0800

    a

--- pip_py3.bat
+++ pip_py3.bat
@@ -6,17 +6,14 @@ call activate
 call python -m pip install --no-cache-dir -U ^
  pip setuptools wheel build tqdm
 call pip install --no-cache-dir -U ^
- jupyterlab numpy pandas pyarrow openpyxl matplotlib seaborn dash ^
- networkx apache-beam scikit-learn xgboost statsmodels prophet ^
- tf-keras tensorflow-cpu tensorflow-probability ortools ^
- scrapy selenium langchain langgraph langchain-chroma transformers
-call pip install --no-cache-dir ^
- torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 ^
- --index-url https://download.pytorch.org/whl/cpu
-call pip install --no-cache-dir ^
- torch_geometric pyg_lib torch_scatter torch_sparse ^
- torch_cluster torch_spline_conv ^
- -f https://data.pyg.org/whl/torch-2.3.1+cpu.html
+ jupyterlab numpy pandas pyarrow openpyxl ^
+ matplotlib seaborn dash networkx apache-beam ^
+ scikit-learn xgboost statsmodels prophet ^
+ tf-keras tensorflow-cpu tensorflow-probability ^
+ torch torchvision torchaudio torch_geometric ^
+ langchain langchain-community langgraph ^
+ langchain-chroma langchain-huggingface ^
+ transformers ortools selenium pypdf
 call pip check
 call deactivate
 call cd C:\Users\Public\py3_rar
