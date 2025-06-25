52f9cd1a0116a02fc3634a698e3ff54cf459b38d
unknown <darklemon2000@gmail.com>
2025-06-25 18:32:34 +0800

    pyro

--- pip_py3.bat
+++ pip_py3.bat
@@ -3,17 +3,9 @@ call cd C:\Users\Public\py3
 call python -m venv venv
 call cd C:\Users\Public\py3\venv\Scripts
 call activate
-call python -m pip install --no-cache-dir -U ^
- pip setuptools wheel build tqdm
+call python -m pip install --no-cache-dir -U pip
 call pip install --no-cache-dir -U ^
- jupyterlab numpy pandas pyarrow openpyxl ^
- matplotlib seaborn dash networkx apache-beam ^
- scikit-learn xgboost statsmodels prophet ^
- tf-keras tensorflow-cpu tensorflow-probability ^
- torch torchvision torchaudio torch_geometric ^
- langchain langchain-community langgraph ^
- langchain-chroma langchain-huggingface ^
- transformers ortools selenium pypdf
+ pyro-ppl
 call pip check
 call deactivate
 call cd C:\Users\Public\py3_rar
